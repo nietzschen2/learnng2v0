6367e1bcb022d108f4b78002bb6bcf8306c02f86
nietzschen2 <alastair@alastairclegg.me>
2016-06-10 10:56:46 +0100

    project setup

---
+++ APM/app/main.ts
@@ -0,0 +1,7 @@
+import { bootstrap } from 'angular2/platform/browser';
+// this is not importing from angular core because features are different depending on the platform
+
+// Our main component
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
