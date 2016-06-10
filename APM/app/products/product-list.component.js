312d1f7f2b107eb65abe58e35f6df1f893f3a093
nietzschen2 <Alastair@Alastairs-MacBook-Pro.local>
2016-06-10 14:35:05 +0100

    included filter by function + added pipes to product code and currency

--- APM/app/products/product-list.component.js
+++ APM/app/products/product-list.component.js
@@ -24,6 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                     this.imageWidth = 50;
                     this.imageMargin = 2;
                     this.showImage = false;
+                    this.listFilter = "cart";
                     this.products = [
                         {
                             "productId": 1,
