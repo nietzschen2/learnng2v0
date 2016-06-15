69832270625c50d145db7dfab0b8ba53489d63e2
nietzschen2 <Alastair@Alastairs-MacBook-Pro.local>
2016-06-15 17:21:05 +0100

    added a custom pipe for the product filter

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from 'angular2/core';
 import { IProduct } from './product';
+import { ProductFilterPipe } from './product-filter.pipe'
 
 @Component({
   selector: 'pm-products',
   templateUrl: 'app/products/product-list.component.html',
-  styleUrls: ['app/products/product-list.component.css']
+  styleUrls: ['app/products/product-list.component.css'],
+  pipes: [ProductFilterPipe]
 })
 
 export class ProductListComponent implements OnInit {
